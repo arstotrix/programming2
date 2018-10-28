8e9d1c4a72e0d447b60667e8d853df21d3011314
Marie <mashamaslova85@gmail.com>
2018-10-29 01:44:25 +0300

    23456789098765432

---
+++ homeworks/hw3/_2_corpusmaker.py
@@ -0,0 +1,37 @@
+import os
+import re
+import urllib.request
+import json
+import time
+from bs4 import BeautifulSoup
+
+def findarticles():
+    article_data = []
+    with open ("articles.txt", 'r', encoding = 'utf-8') as f:
+        articles = json.load(f, ensure_ascii = False)
+    for a in articles:
+        soup = bs4.BeautifulSoup(a)
+        article_text = soup.find('td', {'class': "center-content"})
+        article_date = article_text.find("span", {"class": "news-date"}).get_text()
+        article_heading = article_text.find("h1").get_text()
+        article_article = article_text.find("div", {"class":"news-content"}).get_text()
+
+        article_text = re.sub("<.*?>","",article_text)
+        article_date = re.sub("<.*?>","",article_date)
+        article_heading = re.sub("<.*?>","",article_heading)
+        article_article = re.sub("<.*?>","",article_article)
+
+        article_text = html.unescape(article_text)
+        article_date = html.unescape(article_date)
+        article_heading = html.unescape(article_heading)
+        article_article = html.unescape(article_article)
+
+        article_data.append({'heading':article_heading, 'date': article_date, "article": article_article})
+    return article_data
+
+def main():
+    articles = findarticles()
+    
+if __name__ == "__main__":
+    main()
+    
