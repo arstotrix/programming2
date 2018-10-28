39a366e272c117fca7fcd313fb0621c3c6df157b
Marie <mashamaslova85@gmail.com>
2018-10-29 01:59:56 +0300

    key

--- homeworks/hw3/_2_corpusmaker.py
+++ homeworks/hw3/_2_corpusmaker.py
@@ -9,13 +9,13 @@ def findarticles():
     article_data = []
     with open ("articles.txt", 'r', encoding = 'utf-8') as f:
         articles = json.load(f, ensure_ascii = False)
-    for a in articles:
+    for a, key in articles.items():
         soup = bs4.BeautifulSoup(a)
         article_text = soup.find('td', {'class': "center-content"})
         article_date = article_text.find("span", {"class": "news-date"}).get_text()
         article_heading = article_text.find("h1").get_text()
         article_article = article_text.find("div", {"class":"news-content"}).get_text()
-
+        
         article_text = re.sub("<.*?>","",article_text)
         article_date = re.sub("<.*?>","",article_date)
         article_heading = re.sub("<.*?>","",article_heading)
@@ -26,7 +26,7 @@ def findarticles():
         article_heading = html.unescape(article_heading)
         article_article = html.unescape(article_article)
 
-        article_data.append({'heading':article_heading, 'date': article_date, "article": article_article})
+        article_data.append({'heading':article_heading, 'date': article_date, "article": article_article}, 'id': key)
     return article_data
 
 def main():
