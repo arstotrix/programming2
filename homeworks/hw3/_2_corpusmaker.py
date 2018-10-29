cddba511b4a01fded9958312b4629269da9f8874
Marie <mashamaslova85@gmail.com>
2018-10-29 03:09:50 +0300

    maker 4

--- homeworks/hw3/_2_corpusmaker.py
+++ homeworks/hw3/_2_corpusmaker.py
@@ -11,24 +11,28 @@ def findarticles():
     with open ("articles.txt", 'r', encoding = 'utf-8') as f:
         articles = json.load(f)
     for key, a in articles.items():
-        soup = BeautifulSoup(a, 'html-parser')
-        article_text = soup.find('td', {'class': "center-content"})
-        article_date = article_text.find("span", {"class": "news-date"}).get_text()
-        article_heading = article_text.find("h1").get_text()
-        article_article = article_text.find("div", {"class":"news-content"}).get_text()
+        try:
+            print(key, a)
+            soup = BeautifulSoup(a, 'html.parser')
+            article_text = soup.find('td', {'class': "center-content"})
+            article_date = article_text.find("span", {"class": "news-date"}).get_text()
+            article_heading = article_text.find("h1").get_text()
+            article_article = article_text.find("div", {"class":"news-content"}).get_text()
         
-        article_text = re.sub("<.*?>","",article_text)
-        article_date = re.sub("<.*?>","",article_date)
-        article_heading = re.sub("<.*?>","",article_heading)
-        article_article = re.sub("<.*?>","",article_article)
+            article_text = re.sub("<.*?>","",article_text)
+            article_date = re.sub("<.*?>","",article_date)
+            article_heading = re.sub("<.*?>","",article_heading)
+            article_article = re.sub("<.*?>","",article_article)
 
-        article_text = html.unescape(article_text)
-        article_date = html.unescape(article_date)
-        article_heading = html.unescape(article_heading)
-        article_article = html.unescape(article_article)
+            article_text = html.unescape(article_text)
+            article_date = html.unescape(article_date)
+            article_heading = html.unescape(article_heading)
+            article_article = html.unescape(article_article)
 
-        print(article_heading, article_article, article_date)
-        article_data.append({'heading':article_heading, 'date': article_date, "article": article_article, 'id': key})
+            print(article_heading, article_article, article_date)
+            article_data.append({'heading':article_heading, 'date': article_date, "article": article_article, 'id': key})
+        except Exception:
+            print(a)
     return article_data
 
 def main():
